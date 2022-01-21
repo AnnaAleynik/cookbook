808d0726faf993f70449ee4ca6de9fe46e7a87bd
AnnaAleynik <anna.mikhailovna305@gmail.com>
2022-01-21 18:17:27 +0300

    add rubocop

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   extend Enumerize
 
-  AVAILABLE_STATUSES = %w(pending denied moderating approved)
+  AVAILABLE_STATUSES = %w(pending denied moderating approved).freeze
 
   enumerize :status, in: AVAILABLE_STATUSES, predicates: true, default: :pending
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/profile'
+  get "users/profile"
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: "users/registrations"
   }
   get "/recipes", to: "recipes#index"
 end
