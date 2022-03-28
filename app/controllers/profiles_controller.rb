dd446b00ad52fa1a3418e132b1f4d2be11123224
AnnaAleynik <anna.aleynik@flatstack.com>
2022-03-28 16:38:02 +0300

    Profile features (#9)
    * wip
    
    * fix
    
    * fix
    
    * fix style

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,21 @@
+class ProfilesController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(users_params)
+      redirect_to profile_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:email, :avatar)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   root to: "home#show"
   devise_for :users, controllers: { registrations: "users/registrations" }
 
+  resource :profile, only: %i(show edit update)
+
   resources :recipes, only: %i(index show)
   resources :ingredients
 
   namespace :users do
-    resources :recipes, only: %i(new create edit update destroy), module: :users
+    resources :recipes, only: %i(new create edit update destroy)
   end
 end
