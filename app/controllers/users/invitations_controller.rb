a0a8beb9b5966049e94b693304dde3cd2be9dc3a
AnnaAleynik <anna.aleynik@flatstack.com>
2022-04-19 12:07:24 +0300

    add policy

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,6 +1,6 @@
 module Users
   class InvitationsController < Devise::InvitationsController
-    before_action :configure_permitted_parameters
+    before_action :configure_permitted_parameters, :authorize_resource!
     # before_action :authorize_user
 
     def create
@@ -10,11 +10,14 @@ module Users
     protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:invite, keys: %w(first_name last_name roles))
+      devise_parameter_sanitizer.permit(:invite, keys: %w(first_name last_name login roles))
     end
 
     # def authorize_user
     #   authorize! current_user, to: :admin?
     # end
+    def authorize_resource!
+      authorize! current_user, with: InvitationPolicy
+    end
   end
 end
